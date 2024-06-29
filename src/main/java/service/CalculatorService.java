f18015e17011163c36aa31fd2bcbea005384dd57
Pavel <pavelpulle1@gmail.co>
2024-06-29 22:47:15 +0300

    Домашка готова

---
+++ src/main/java/service/CalculatorService.java
@@ -0,0 +1,32 @@
+package service;
+
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Service
+
+public class CalculatorService {
+
+
+    public int getSummary(int num1, int num2) {
+        return num1 + num2;
+    }
+
+
+
+    public int getSubstraction(int num1, int num2) {
+        return num1 - num2;
+    }
+
+
+    public int getMultiply(int num1, int num2) {
+        return num1 * num2;
+    }
+
+
+    public int getDivision(int num1, int num2) {
+        return num1 / num2;
+    }
+}
